e7d09717fa7d3e53b87c98a420af120149d120b7
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-07-30 19:00:42 +0530

    feat: created a mobile sidebar, created a sticky navbar with color transition, created both navbar and footer component, installed shadcn/ui for developing UI components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <section className='w-full bg-slate-900'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam magni sed maxime ducimus ipsum a, saepe iste harum deleniti velit quisquam non consequuntur, incidunt quia, id voluptates fugiat quam dolore. Minus nobis numquam rerum. Quisquam pariatur est commodi deserunt molestias, vel, doloribus fugit ullam blanditiis incidunt magni eligendi impedit nostrum eum voluptas dolores aspernatur autem, iusto quibusdam voluptates perspiciatis corrupti eos? Doloribus labore dignissimos repudiandae numquam architecto! Suscipit exercitationem architecto maxime dicta, ipsam laborum consectetur, labore asperiores autem beatae fuga. Ducimus sit, ipsam eos voluptatum similique sapiente animi aliquam quis deserunt amet totam dolorem dolorum numquam voluptatibus aspernatur vel veritatis voluptas dignissimos, dolor magni perspiciatis nisi nulla debitis atque. Consectetur possimus voluptatem sunt sit autem illo mollitia reiciendis eligendi deleniti voluptatum, ipsum eius nemo omnis ipsa quaerat maxime laudantium iste ex. Ex, recusandae? Odit corporis maiores voluptatum, fugit, blanditiis numquam facere quam explicabo aspernatur veniam, deleniti fuga consequatur repudiandae facilis reiciendis? Voluptate, mollitia sunt fugiat, repellendus dignissimos temporibus corrupti vel pariatur quas eligendi consequatur iusto magni aperiam nihil sequi praesentium est, neque dolorum reiciendis ullam. Incidunt quae rerum vero expedita? Dignissimos praesentium possimus neque eum voluptatum aut doloremque mollitia beatae ipsum, accusamus deserunt enim dolores repudiandae earum rerum distinctio itaque!
+      </section>
+    </>
+  )
+}
+
+export default Footer
