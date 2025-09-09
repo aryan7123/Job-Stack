7fde1c07768c0e7a644236584e459cbcf33a2762
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-09-09 22:58:11 +0530

    feat: created a job detail component

---
+++ app/job-details/[id]/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const page = ({ params }: { params: { id: string } }) => {
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default page
