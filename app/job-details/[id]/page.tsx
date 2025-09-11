78bf1289940a7df81ef2bb152c24425ee26f93ef
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-09-11 18:26:39 +0530

    feat: completed the job details page

--- app/job-details/[id]/page.tsx
+++ app/job-details/[id]/page.tsx
@@ -81,10 +81,57 @@ const page = ({ params }: { params: Promise<{ id: string }> }) => {
                                 <h3 className='text-lg font-semibold'>Qualifications & Skills</h3>
                                 <p className='mt-4 text-slate-400 whitespace-pre-line'>{data?.details.qualification}</p>
                             </div>
+                            <button type="button" className='mt-6 text-white rounded-md bg-emerald-600 text-base font-semibold tracking-wide py-2 px-4 transition-colors duration-500 hover:bg-emerald-700 cursor-pointer'>Apply Now</button>
                         </div>
                         <div className='lg:col-span-4 md:col-span-6'>
                             <div className='shadow-sm shadow-gray-200 rounded-md bg-white sticky top-20'>
-
+                                <div className='p-6'>
+                                    <h3 className='text-lg font-semibold'>Job Information</h3>
+                                </div>
+                                <div className='border-t border-slate-200 p-6'>
+                                    <ul className='list-none'>
+                                        <li className='flex items-center'>
+                                            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="size-5" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><polyline points="17 11 19 13 23 9"></polyline></svg>
+                                            <div className="ms-4">
+                                                <p className="font-medium">Employee Type:</p>
+                                                <span className="text-emerald-600 font-medium text-sm">{data?.details.type}</span>
+                                            </div>
+                                        </li>
+                                        <li className='flex items-center mt-3'>
+                                            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="size-5" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
+                                            <div className="ms-4"><p className="font-medium">Location:</p><span className="text-emerald-600 font-medium text-sm">{data?.details.location}</span></div>
+                                        </li>
+                                        <li className="flex items-center mt-3">
+                                            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="size-5" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line>
+                                            </svg>
+                                            <div className="ms-4">
+                                                <p className="font-medium">Job Title:</p>
+                                                <span className="text-emerald-600 font-medium text-sm">{data?.details.title}</span>
+                                            </div>
+                                        </li>
+                                        <li className='flex items-center mt-3'>
+                                            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="size-5" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect><path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path></svg>
+                                            <div className="ms-4">
+                                                <p className="font-medium">Experience:</p>
+                                                <span className="text-emerald-600 font-medium text-sm">{data?.details.experience} Years</span>
+                                            </div>
+                                        </li>
+                                        <li className='flex items-center mt-3'>
+                                            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="size-5" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+                                            <div className="ms-4">
+                                                <p className="font-medium">Salary:</p>
+                                                <span className="text-emerald-600 font-medium text-sm">${data?.details.salary}</span>
+                                            </div>
+                                        </li>
+                                        <li className='flex items-center mt-3'>
+                                            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="size-5" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
+                                            <div className="ms-4">
+                                                <p className="font-medium">Date Posted:</p>
+                                                <span className="text-emerald-600 font-medium text-sm">{formatDistanceToNow(data?.details.postedAt, {addSuffix: true})}</span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
