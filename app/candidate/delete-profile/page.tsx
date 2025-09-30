8ca0483c6aba47e295a1f2d9cad12c9ae63639d6
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-09-30 23:12:09 +0530

    feat: creating the modal for deleting the profile of the candidate

--- app/candidate/delete-profile/page.tsx
+++ app/candidate/delete-profile/page.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
+import { useSession } from 'next-auth/react';
+import Loader from '@/components/ui/Loader';
 
 const page = () => {
+  const { data: session } = useSession();
+  const userId = session?.user?.id ?? '';
+
   return (
-    <div>page</div>
+    <>
+      <section className="w-full relative">
+        <div className="w-full px-0 sm:px-6 md:px-12 lg:px-20">
+          <div className="flex flex-col gap-3 mt-5 px-4 sm:px-0">
+            <h2 className="text-4xl font-semibold">Howdy, {session?.user?.name}!!</h2>
+            <span className="text-slate-400 text-sm font-medium">
+              Ready to jump back in?
+            </span>
+          </div>
+          <div className='bg-white mt-10 shadow rounded-md w-[inherit] p-6'>
+            
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
