376d0ff9c0c8ddaa5ce557d51a74331e9b0f8559
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-10-08 23:53:51 +0530

    fix: cookie not removed from the session when deleting the profile

--- app/api/(candidate)/delete-candidate-profile/route.ts
+++ app/api/(candidate)/delete-candidate-profile/route.ts
@@ -1,24 +1,32 @@
-'use server';
+"use server";
 
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function POST(req: NextRequest) {
-    try {
-        const body = await req.json();
+  try {
+    const body = await req.json();
 
-        const deleteCandidateProfile = await prisma.profile.delete({
-            where: {
-                userId: body.candidateId
-            },
-            include: {
-                user: true
-            }
-        });
+    const deleteCandidateProfile = await prisma.profile.delete({
+      where: {
+        userId: body.candidateId,
+      },
+      include: {
+        user: true,
+      },
+    });
 
-        return NextResponse.json({ message: "Candidate Deleted Successfully", deleteCandidateProfile });
+    await prisma.user.delete({
+      where: {
+        id: deleteCandidateProfile.userId,
+      },
+    });
 
-    } catch (error) {
-        console.log(error);
-    }
+    return NextResponse.json({
+      message: "Candidate Deleted Successfully",
+      deleteCandidateProfile,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
